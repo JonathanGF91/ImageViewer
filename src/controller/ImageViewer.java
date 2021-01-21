b3463e60a4172a8f260f06b927a9a6abee0ea140
Jonathan Gómez <joni.gf91@gmail.com>
2021-01-21 19:49:17 +0000

    image viewer v1.0

---
+++ src/controller/ImageViewer.java
@@ -0,0 +1,19 @@
+
+package controller;
+
+import java.io.File;
+import model.Image;
+import persistence.FileImageLoader;
+
+public class ImageViewer {
+
+
+    public static void main(String[] args) {
+        
+        File file = new File(".\\image");
+        FileImageLoader imageLoader = new FileImageLoader(file);
+        Image image = imageLoader.load();
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.getImageDisplay().show(image);
+    } 
+}
